72f895270a63e79b3fee081d37e0783fe7e24449
Evan BOISSONNOT <evan@boissonnot.fr>
2021-11-30 17:22:24 +0100

    feat(type): class and interface

--- lessons/learn/07-classes/src/main.ts
+++ lessons/learn/07-classes/src/main.ts
@@ -1,15 +1,31 @@
-// type string
-const message: string = 'Grrrroaaww';
-const message2 = 'Grrrroaaww';
+class Character {
+    constructor(public surname: string) {
 
-// type number
-const result: number = 2;
-const result2 = 2;
+    }
+}
 
-// object
-const wookie = {
-    surname: 'Chewie',
-    size: 275
-};
-// wookie.ship //=> ne compile pas
+class Wookie extends Character {
+    constructor(surname: string) {
+        super(surname);
+    }
 
+    get name(): string {
+        return 'chewie'
+    }
+    set name(value: string) {
+        this.surname = value;
+    }
+}
+
+class Weapon {
+    constructor(power: number); // overloads
+    constructor(power: number, name: string); // overloads
+    constructor(power: number, name?: string) { // implementation
+
+    }
+}
+const weapon = new Weapon(1, 'Blaster');
+
+
+const wookie = new Wookie('ddd');
+wookie.name = 'test';
---
+++ lessons/learn/08-interfaces/src/main.ts
@@ -0,0 +1,67 @@
+class Character {
+    constructor(public surname: string) {
+
+    }
+}
+
+interface Warrior {
+    attak(perso: Character): void;
+    power?: number;
+}
+
+class Wookie extends Character implements Warrior {
+    constructor(public surname: string) {
+        super(surname);
+    }
+
+    attak(perso: Character): void {
+
+    }
+
+    get name(): string {
+        return 'chewie'
+    }
+    set name(value: string) {
+        this.surname = value;
+    }
+}
+
+class Weapon {
+    constructor(power: number); // overloads
+    constructor(power: number, name: string); // overloads
+    constructor(power: number, name?: string) { // implementation
+
+    }
+
+    static shootEmAll() {
+        console.info('pingggg');
+    }
+}
+const weapon = new Weapon(1, 'Blaster');
+
+
+const wookie = new Wookie('ddd');
+wookie.name = 'test';
+
+// wookie.power = 100;
+
+Weapon.shootEmAll();
+
+
+
+// ****** STRANGE BUT REAL IN JS/TS********
+interface WookieStrange {
+    name: string;
+}
+
+class Droid {
+    name !: string;
+}
+
+let wookieDroid !: WookieStrange;
+wookieDroid = new Droid(); // same structure !!
+
+// encore plus loin
+
+let otherDroide = { name: '121121', size: 12 };
+wookieDroid = otherDroide;
