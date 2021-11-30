9f5cd7be4dc4b200902a37af303265254698e7b0
Evan BOISSONNOT <evan@boissonnot.fr>
2021-11-30 16:43:58 +0100

    feat(type): functions

---
+++ lessons/learn/06-functions/src/main.ts
@@ -0,0 +1,86 @@
+function printCoords(point: { x: number, y: number }) {
+    console.info(point);
+}
+
+function printCoordsOpt(point: { x: number, y?: number }) {
+    console.info(point);
+}
+
+printCoords({ x: 1, y: 2 });
+printCoordsOpt({ x: 1, y: 2 });
+printCoordsOpt({ x: 1 });
+
+//-------------------------------------------------------------
+
+function computeSumFn(values: number[], log: Function): number {
+    let result: number = 0;
+    values.forEach(item => result += item);
+
+    log(result);
+
+    return result;
+}
+
+function computeSum(values: number[], log: (message: string | number) => void): number {
+    let result: number = 0;
+    values.forEach(item => result += item);
+
+    log(result);
+
+    return result;
+}
+
+type Logger = (message: string | number) => void;
+function computeSumTyped(values: number[], log: Logger): number {
+    let result: number = 0;
+    values.forEach(item => result += item);
+
+    log(result);
+
+    return result;
+}
+
+
+computeSumFn([1, 5, 7], console.info);
+computeSum([1, 5, 7], console.info);
+computeSumTyped([1, 5, 7], console.info);
+
+interface WithString {
+    name: string;
+    age: number;
+
+    sayHello(): void;
+}
+
+type SomeConstructor = { new (s: string, a: number): WithString };
+   
+class WookieWithString implements WithString {
+    constructor(public name: string, public age: number) {}
+
+    sayHello() {
+        console.info('hello', this.age);
+    }
+}
+   
+
+function makeWookie(ctor: SomeConstructor) {
+  return new ctor("chewie", 18);
+}
+
+const wookie = makeWookie(WookieWithString);
+wookie.sayHello();
+
+
+// -------- OVERLOADS --------------
+function makeDate(timestamp: number): Date; // overload
+function makeDate(m: number, d: number, y: number): Date; // overload
+function makeDate(mOrTimestamp: number, d?: number, y?: number): Date { // implementation
+  if (d !== undefined && y !== undefined) {
+    return new Date(y, mOrTimestamp, d);
+  } else {
+    return new Date(mOrTimestamp);
+  }
+}
+const d1 = makeDate(12345678);
+const d2 = makeDate(5, 5, 5);
+//const d3 = makeDate(1, 3); // pas d'overload ici à deux éléments
---
+++ lessons/learn/07-classes/src/main.ts
@@ -0,0 +1,15 @@
+// type string
+const message: string = 'Grrrroaaww';
+const message2 = 'Grrrroaaww';
+
+// type number
+const result: number = 2;
+const result2 = 2;
+
+// object
+const wookie = {
+    surname: 'Chewie',
+    size: 275
+};
+// wookie.ship //=> ne compile pas
+
